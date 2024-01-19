34c75eb18c3aefbe434db34db2d8ceb85bb9454e
Nataliya Kim <magistr1982@gmail.com>
2024-01-19 17:54:51 +0200

    Add favicon and Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            width={27}
+            height={27}
+          />
+          <p className="nav-logo"> 
+           Price<span className="text-primary">Wise</span>
+          </p>
+        </Link>
+        <div className="flex items-center gap-5">
 
-export default Navbar
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
