0fe6a183c534b02a36c4aeb4cf10beb0b5fadfc7
Nataliya Kim <magistr1982@gmail.com>
2024-01-26 08:02:24 +0200

    Add getAllProducts function

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import HeroCarousel from "@/components/HeroCarousel";
 import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
+import { getAllProducts } from "@/lib/actions";
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts();
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -33,11 +35,9 @@ const Home = () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {["Apple Iphone 15", "Book", "Sneakers", "Laptops"].map(
-            (product, index) => (
-              <div key={index}>{product}</div>
-            )
-          )}
+          {allProducts?.map((product, index) => (
+            <div key={index}>{product.title}</div>
+          ))}
         </div>
       </section>
     </>
