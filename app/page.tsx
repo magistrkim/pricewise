7670b68df2e14c0b4c2e9708e6509048380bc346
Nataliya Kim <magistr1982@gmail.com>
2024-01-26 08:43:36 +0200

    Create ProductCard

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroCarousel from "@/components/HeroCarousel";
 import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 import { getAllProducts } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
 
 const Home = async () => {
   const allProducts = await getAllProducts();
@@ -34,9 +35,9 @@ const Home = async () => {
       </section>
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
-        <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {allProducts?.map((product, index) => (
-            <div key={index}>{product.title}</div>
+        <div className="flex flex-wrap gap-x-8 gap-y-16 justify-center">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
