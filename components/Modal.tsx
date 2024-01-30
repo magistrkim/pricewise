ccaf1259a7d811b5cb526851382f138c896d3278
Nataliya Kim <magistr1982@gmail.com>
2024-01-30 09:32:14 +0200

    Add headless UI lib and Modal component

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,8 +1,46 @@
+"use client";
+import { useState, Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 const Modal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
   return (
-    <div>Modal</div>
-  )
-}
+    <>
+      <button className="btn" type="button" onClick={openModal}>
+        Track
+      </button>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" onClose={closeModal} className="dialog-container">
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="dialog-content">TEST</div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
 
-export default Modal
+export default Modal;
